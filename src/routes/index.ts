764d77a968401b629c24d375678a1b9fe176f642
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-09-14 23:16:40 +0900

    use template engine ejs

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+export const router = express.Router()
+
+router.get(
+  '/',
+  async (req: express.Request, res: express.Response): Promise<void> => {
+    res.render('index')
+  },
+)
