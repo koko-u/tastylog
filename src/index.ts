14673767a62f029c4be1bbb28a887e4a1e375ccb
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-09-14 23:43:34 +0900

    serve favicon

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import { router } from './routes'
-import { resolve } from 'path'
+import path from 'path'
+import favicon from 'serve-favicon'
 
 dotenv.config()
 
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
-app.set('views', resolve(__dirname, 'views'))
+app.set('views', path.resolve(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.use('/public', express.static(resolve(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use('/public', express.static(path.resolve(__dirname, 'public')))
 
 app.use('/', router)
 
