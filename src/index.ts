df2679cb47eb41bbe31f4a3d7f6bbda43b5a94d6
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-09-14 23:30:48 +0900

    serve prepared static files

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ const app = express()
 
 app.set('views', resolve(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+app.use('/public', express.static(resolve(__dirname, 'public')))
+
 app.use('/', router)
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
